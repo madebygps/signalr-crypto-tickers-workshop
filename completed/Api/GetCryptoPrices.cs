924270da449d5337a3ab9f25b51ecc38cacb0e12
Gwyneth Pena-Siguenza <gwynethpena@outlook.com>
2022-12-11 10:33:34 -0500

    Refactored code for null

--- completed/Api/GetCryptoPrices.cs
+++ completed/Api/GetCryptoPrices.cs
@@ -7,51 +7,70 @@ namespace Completed.Api
 {
     public static class GetCryptoPrices
     {
-    
-         
+
+
         [Function("GetCryptoPrices")]
-        public async static Task<MyOutputType> Run([TimerTrigger("0 */1 * * * *", RunOnStartup = true)] MyInfo myTimer, FunctionContext context)
+        public async static Task<MyOutputType> Run([TimerTrigger("0 */2 * * * *", UseMonitor = true)] MyInfo myTimer, FunctionContext context)
         {
-     
-            Coin[] prices;
+
+            Coin[]? prices;
+            MySignalRMessage mySignalRMessage;
+
             var logger = context.GetLogger("GetCryptoPrices");
+            //logger.LogInformation($"C# Timer trigger function executed at: {myTimer.ScheduleStatus?.LastUpdated}");
             logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
             
+    
             using (var httpClient = new HttpClient())
             {
-            
-                var coinData = await httpClient.GetAsync("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");                
+
+                var coinData = await httpClient.GetAsync("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false");
                 var body = await coinData.Content.ReadAsStringAsync();
-                prices = JsonSerializer.Deserialize<Coin[]>(body);              
-                 
-                foreach(var coin in prices)
+                prices = JsonSerializer.Deserialize<Coin[]>(body);
+
+                if (prices is not null)
                 {
-                    logger.LogInformation(coin.symbol);
+                    /*foreach (var coin in prices)
+                    {
+                        logger.LogInformation(coin.symbol);
 
+                    }*/
+
+                    mySignalRMessage = new MySignalRMessage()
+                    {
+                        Target = "updated",
+                        Arguments = new object[] { prices }
+                    };
+                    return new MyOutputType()
+                    {
+                        mySignalRMessage = mySignalRMessage,
+                        CosmosCoins = prices
+                    };
                 }
-            }
+                else
+                {
+                    return new MyOutputType()
+                    {
+                        mySignalRMessage = new MySignalRMessage(),
+                        CosmosCoins = new Coin[1] 
+                    };
+                }
+                
 
-            MySignalRMessage mySignalRMessage = new MySignalRMessage()
-            {
-                Target = "updated",
-                Arguments =  new object[]{prices} 
-            };
-            //logger.LogInformation(mySignalRMessage.Arguments.Length.ToString());
+            }
+        
             
-            return new MyOutputType()
-        {
-            mySignalRMessage = mySignalRMessage,
-            CosmosCoins = prices
-        };
+
         
         }
+
+        
     }
 
-    
+
     public class MyInfo
     {
-        public MyScheduleStatus ScheduleStatus { get; set; }
+        public MyScheduleStatus? ScheduleStatus { get; set; }
     }
 
     public class MyScheduleStatus
@@ -65,16 +84,16 @@ namespace Completed.Api
     public class MyOutputType
     {
         [SignalROutput(HubName = "stocks", ConnectionStringSetting = "AzureSignalRConnectionString")]
-        public MySignalRMessage mySignalRMessage {get;set;}
+        public MySignalRMessage? mySignalRMessage { get; set; }
 
         [CosmosDBOutput("%DatabaseName%", "%CollectionName%", ConnectionStringSetting = "CosmosDBConnectionString")]
-        public Coin[] CosmosCoins {get;set;}
-       
+        public Coin[]? CosmosCoins { get; set; }
+
     }
     public class MySignalRMessage
     {
-        public string Target { get; set; }
+        public string? Target { get; set; }
 
-        public object Arguments { get; set; }
+        public object? Arguments { get; set; }
     }
 }
